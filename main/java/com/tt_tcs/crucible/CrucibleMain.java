74503d78846eb356e619206b9d57b254b202da6c
TT-TCS <thomashughes222@hotmail.com>
2026-01-25 13:37:25 +0000

    Removed UpdateChecker.java

--- main/java/com/tt_tcs/crucible/CrucibleMain.java
+++ main/java/com/tt_tcs/crucible/CrucibleMain.java
@@ -14,7 +14,6 @@ import com.tt_tcs.crucible.drugs.DrugUsing;
 import com.tt_tcs.crucible.util.CrucibleCommands;
 import com.tt_tcs.crucible.util.CrucibleListener;
 import com.tt_tcs.crucible.util.StopwatchManager;
-import com.tt_tcs.crucible.util.UpdateChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
@@ -27,8 +26,6 @@ public final class CrucibleMain extends JavaPlugin {
     private DrugProcessingManager processingManager;
     private ChemicalProcessorManager chemicalProcessorManager;
 
-    
-    private final UpdateChecker updateChecker = new UpdateChecker();
 public void startToleranceDecay() {
         Bukkit.getScheduler().runTaskTimer(this, () -> {
             for (Player player : Bukkit.getOnlinePlayers()) {
@@ -61,12 +58,7 @@ public void startToleranceDecay() {
             getDataFolder().mkdirs();
         }
 
-        // Register event listeners
-        
-        // Check GitHub releases for updates (admins will be notified on join)
-        updateChecker.checkAsync(this);
-
-getServer().getPluginManager().registerEvents(new WeedCropManager(), this);
+        getServer().getPluginManager().registerEvents(new WeedCropManager(), this);
         getServer().getPluginManager().registerEvents(new CocaineCropManager(), this);
         getServer().getPluginManager().registerEvents(new EphedraCropManager(), this);
         getServer().getPluginManager().registerEvents(new PsilocybeCropManager(), this);
@@ -107,10 +99,6 @@ getServer().getPluginManager().registerEvents(new WeedCropManager(), this);
 
     public static CrucibleMain getInstance() {
         return instance;
-    }
-
-    public UpdateChecker getUpdateChecker() {
-        return updateChecker;
     }
 
 }
